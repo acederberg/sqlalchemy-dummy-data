3c5aa6b3385c2d34567eeac2ccc1cb211730e18a
Adrian Cederberg <adrian@mountainvector.com>
2023-08-21 10:31:50 -0600

    Finished stuff for _create_iter_pks after doing some math. will write down as latex later, a few more tests as results of this will be implemented.

--- sqlalchemy_dummy_data/seq.py
+++ sqlalchemy_dummy_data/seq.py
@@ -1,44 +1,85 @@
 import itertools
-from typing import Generator, Iterable, List, Tuple
+from typing import Dict, Generator, Iterable, List, Optional, Sequence, Tuple
+
+
+def unique_permutations(
+    elements: Sequence,
+) -> Generator[Tuple[int, ...], None, None,]:
+    if len(elements) == 1:
+        yield (elements[0],)
+    else:
+        unique_elements = set(elements)
+        for first_element in unique_elements:
+            remaining_elements = list(elements)
+            remaining_elements.remove(first_element)
+            for sub_permutation in unique_permutations(remaining_elements):
+                yield (first_element, *sub_permutation)
 
 
 class iters:
     @classmethod
-    def count(cls, start: int = None, stop: int = None) -> Generator[int, None, None]:
-        k = start or 1
+    def count(
+        cls, start: Optional[int] = None, stop: Optional[int] = None
+    ) -> Generator[int, None, None]:
+        """Sould 'work like' range except with indefinite iteration.
+
+        :param start: 1 if not overridden.
+        :param stop: Iteration will terminate at this value if specified.
+        """
+
+        start = start or 1
+        if stop is not None and start > stop:
+            msg = "`start` parameter must be less than or equal to `stop` "
+            msg += "parameter."
+            raise ValueError(msg)
+
+        k = start
         while True:
             yield k
-            if stop is not None and start >= stop:
+            if stop is not None and k >= stop:
                 continue
             k += 1
 
     @classmethod
-    def triangled(cls, *items: Iterable[int]) -> Iterable[Tuple[int, ...]]:
-        """Iterate"""
+    def _triangled(
+        cls, *items: Iterable[int]
+    ) -> Generator[Tuple[int, ...], None, None]:
+        """Iterate the members of the product of the items such that they act
+        like the sequenct described in :meth:`DummyMixins._create_iter_pks`
+        when :param:`items` is made of similar items.
+
+        :param items: The things to make the product from.
+        :returns: See function description.
+        """
         n = len(items)
         if not n:
             raise ValueError("Arguments must have length of 1 or greater.")
         elif n == 1:
-            return items[0]
-        elif n == 2:
-            yield from (
-                (a, b)
-                for k, a in enumerate(items[0])
-                for j, b in enumerate(items[1])
-                if j <= k
-            )
+            yield from ((item,) for item in items[0])
             return
-        first_item: Iterable[int]
+
+        last_item: Iterable[int]
         remaining_items: List[Iterable[int]]
-        first_item, *remaining_items = items
-        for first in first_item:
-            for item in cls.triangled(*remaining_items):
-                yield (
-                    first,
-                    *item,
-                )
+        last_item, *remaining_items = items
+        for last in last_item:
+            for item in cls._triangled(*remaining_items):
+                if item[-1] <= last:
+                    yield (*item, last)
 
     @classmethod
-    def squared(cls, *items: Iterable):
-        for item in cls.triangled(*items):
-            yield from itertools.permutations(item)
+    def _squared(cls, *items: Iterable):
+        for item in cls._triangled(*items):
+            yield from unique_permutations(item)
+
+    @classmethod
+    def triangled(
+        cls,
+        *labels,
+        start: Optional[int] = None,
+        stop: Optional[int] = None,
+    ) -> Iterable[Dict[str, int]]:
+        counters = {label: cls.count(start, stop) for label in labels}
+        yield from (
+            {k: v for k, v in zip(counters, coord)}
+            for coord in cls._triangled(*counters.values())
+        )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,19 +4,19 @@ from sqlalchemy_dummy_data.seq import iters
 class Test:
     def test_triangle_iter(self):
         # 2d is easy to verify by comparison.
-        d2 = list(iters.triangled(range(1, 4), range(1, 4)))
-        assert d2 == [(1, 1), (2, 1), (2, 2), (3, 1), (3, 2), (3, 3)]
+        d2 = list(iters._triangled(range(1, 4), range(1, 4)))
+        assert d2 == [(1, 1), (1, 2), (2, 2), (1, 3), (2, 3), (3, 3)]
 
         # For 3d verify uniqueness and length. Every member of the product
         for k in range(3, 7):
             coproduct = (range(1, 4) for _ in range(k))
-            d3 = list(iters.triangled(*coproduct))
+            d3 = list(iters._triangled(*coproduct))
             assert len(set(item for item in d3)) == (n := len(d3))
-            assert n == int(4 * (3 ** (k - 1)) / 2)
+            # assert n == int(4 * (3 ** (k - 1)) / 2)
 
     def test_squares(self):
         # This test may be slow. The results will be used lazily.
         for k in range(2, 8):
             coproduct = (range(1, 4) for _ in range(k))
-            d3 = tuple(iters.squared(*coproduct))
-            assert len(set(d3)) == 3**k == len(d3)
+            d3 = tuple(iters._squared(*coproduct))
+            assert len(set(d3)) == 3**k == len(d3), str(d3)
